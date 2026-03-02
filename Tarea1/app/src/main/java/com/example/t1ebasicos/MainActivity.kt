7ecf91ddce19038d504ae2ffdebaa92f4d60784e
Esteban Rios <192398777+stebancito@users.noreply.github.com>
2026-03-01 23:34:57 -0600

    T1: Creacion de proyecto "Tarea1" y dos primeros fragments

--- Tarea1/app/src/main/java/com/example/t1ebasicos/MainActivity.kt
+++ Tarea1/app/src/main/java/com/example/t1ebasicos/MainActivity.kt
@@ -5,16 +5,55 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+//        bottomNavigationView = findViewById(R.id.bottom_navigation)
+//
+//        // Cargar el fragment inicial (por ejemplo, TextFieldsFragment)
+//        if (savedInstanceState == null) {
+//            loadFragment(frag1())
+//            bottomNavigationView.selectedItemId = R.id.navigation_textfields
+//        }
+//
+//        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.navigation_textfields -> {
+//                    loadFragment(frag1())
+//                    true
+//                }
+//                R.id.navigation_buttons -> {
+//                    loadFragment(frag2())
+//                    true
+//                }
+//                R.id.navigation_selection -> {
+//                    loadFragment(frag2())
+//                    true
+//                }
+//                R.id.navigation_lists -> {
+//                    loadFragment(frag2())
+//                    true
+//                }
+//                R.id.navigation_info -> {
+//                    loadFragment(frag2())
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
